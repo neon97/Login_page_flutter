5a5fe907f838eabeb76cd91991a6dddc678ef32e
Vishwakarma Raj <34263131+neon97@users.noreply.github.com>
2019-08-26 12:41:55 +0530

    Add files via upload

---
+++ 2nd_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Next extends StatefulWidget {
+  @override
+  _NextState createState() => _NextState();
+}
+
+class _NextState extends State<Next> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page"),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("You need to work here !!")
+          ],
+        ),
+      ),
+    );
+  }
+}
